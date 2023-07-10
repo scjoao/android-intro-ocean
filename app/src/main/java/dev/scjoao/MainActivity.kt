1ece996a6fa9c7d702e3bcc1a724dbb5957894c6
João Carlos <joao.carlos_25@hotmail.com>
2023-07-10 13:46:32 -0300

    Initial commit

---
+++ app/src/main/java/dev/scjoao/MainActivity.kt
@@ -0,0 +1,25 @@
+package dev.scjoao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome)
+
+        btEnviar.setOnClickListener{
+            if (etNome.text.isEmpty()){
+                etNome.error = "Digite o nome!"
+            }
+            tvResultado.text = etNome.text
+        }
+    }
+}
